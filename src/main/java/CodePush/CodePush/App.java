e209297ef548b664db6520d29d1d4ff533b424d7
Manoj <manojmurugesan43@gmail.com>
2026-01-05 10:35:42 +0530

    EST Commit

--- src/main/java/CodePush/CodePush/App.java
+++ src/main/java/CodePush/CodePush/App.java
@@ -7,5 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World two!");
+        System.out.println("Test 3");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
     }
 }
--- src/main/java/CodePush/CodePush/SeleniumAPI.java
+++ src/main/java/CodePush/CodePush/SeleniumAPI.java
@@ -1,14 +1,15 @@
 package CodePush.CodePush;
 
-public class RestApi {
+public class SeleniumAPI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Aded a new class");
-		
+		System.out.println("Test 1");
+		System.out.println("Test 2");
 		System.out.println("Test 3");
 		System.out.println("Test 4");
 		System.out.println("Test 5");
+		
 
 	}
 
