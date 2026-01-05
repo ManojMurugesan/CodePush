5e3651dbeff57e962eedf3f11662cee24af12e6d
Manoj <manojmurugesan43@gmail.com>
2026-01-05 12:17:12 +0530

    Develop

---
+++ src/main/java/CodePush/CodePush/Develop.java
@@ -0,0 +1,23 @@
+package CodePush.CodePush;
+
+import org.testng.annotations.Test;
+
+public class Develop {
+	
+	 @Test
+	    public void testLogin() {
+	        System.out.println("Test 1: Login test executed");
+	    }
+
+	    @Test
+	    public void testAddToCart() {
+	        System.out.println("Test 2: Add to cart test executed");
+	    }
+
+	    @Test
+	    public void testLogout() {
+	        System.out.println("Test 3: Logout test executed");
+	    }
+
+
+}
